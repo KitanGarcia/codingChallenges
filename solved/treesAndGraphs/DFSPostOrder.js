d3e3a9d563548f46e32296fbe2ce36b7a7625d8d
Kitan Garcia <kitangar@gmail.com>
2020-12-14 22:37:08 -0800

    solve DFSPostOrder

--- solved/treesAndGraphs/DFSPostOrder.js
+++ solved/treesAndGraphs/DFSPostOrder.js
@@ -2,38 +2,36 @@
  * Using recursion or iteration with a while loop, write a function that takes the root of a binary tree node and outputs an array of values ordered by depth first search
  */
 
-function DFSPreOrder(root) {
+function DFSPostOrder(root) {
   if (!root) {
     return [];
   }
-
+  
   let result = [];
   function traverse(curr) {
     if (!curr) {
       return null;
     }
-
-    result.push(curr.val);
     traverse(curr.left);
     traverse(curr.right);
+    result.push(curr.val);
   }
-
   traverse(root);
   return result;
 }
 
-//FASTER PURE RECURSION SOLUTION
-function preorderTraversal(root, result = []) {
+//PURE RECURSION SOLUTION
+function postOrderTraversal(root, result = []) {
   if (!root) {
     return [];
   }
-  result.push(root.val);
-  
+
   if (root.left) {
-    preorderTraversal(root.left, result);
+    postOrderTraversal(root.left, result)
   }
   if (root.right) {
-    preorderTraversal(root.right, result)
+    postOrderTraversal(root.right, result)
   }
+  result.push(root.val);
   return result;
 }
