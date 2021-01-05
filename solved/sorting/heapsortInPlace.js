a4df25e7bcb763f559668116416ceb05f8ecf2e0
Kitan Garcia <kitangar@gmail.com>
2021-01-04 20:46:33 -0800

    complete mergesort and in place heapsort

--- solved/sorting/heapsortInPlace.js
+++ solved/sorting/heapsortInPlace.js
@@ -4,86 +4,8 @@
 //add items to heap using swaps
 //then remove all values
 //use max heap to have values at the end when we remove from the heap??????????????
-//
-//
-//
-/*
-function heapsort(arr) {
-  //we know heap condition is met when there is only one item in it
-  //start at 1 to represent index 0
-  let heapLength = 1;
-
-  function getParent(child) {
-    return Math.floor((child - 1) / 2);
-  }
-
-  function getChild(parent) {
-    let left = 2 * parent + 1;
-
-    //if left is last item or larger than right
-    if (left >= heapLength - 1 || arr[left] >= arr[left] + 1) {
-      return left;
-    }
-    return left + 1; //right index;
-
-  }
-
-  //build heap one index at a time until arr.length is reached
-  while (heapLength < arr.length) {
-    heapLength++;
-
-    //set parent and child indices to keep track of where we are
-    //start at last index
-    let child = heapLength - 1;
-    let parent = getParent(child);
-
-    //bubble up will perform swaps on parent if child is greater until we get to the top of the heap
-    //or the parent is greater than the child
-    //this continues iterating until heap condition is met
-    
-    //move the largest value to the 0 index and make sure each parent is larger than the child; heapifying
-    while (child > 0 && arr[parent] < arr[child]) {
-      [ arr[parent], arr[child] ] = [ arr[child], arr[parent] ];
-      
-      //update child with parent and get parent of updated child to continue checking/heapifying
-      child = parent;
-      parent = getParent(child);
-    }
-  }
-
-  //now we have a heap with max value at 0 and each parent is larger than its children
-  //now we can sort
-
-
-  //loop to remove each item from heap
-  //since largest value is at 0, we swap with the last index to have largest value last
-  while (heapLength > 1) {
-    //heaps can only be added to and removed from the last position
-    [ arr[0], arr[heapLength - 1] ] = [ arr[heapLength - 1], arr[0] ];
-    heapLength--; //decrease length so largest values are always at the end
-
-    //perform bubble down for new top to satisfy heap condition (children smaller than parents)
-    let parent = 0;
-    let child = getChild(parent); //get index of larger of the 2 children
-    while (child < heapLength && arr[parent] < arr[child]) {
-      [ arr[parent], arr[child] ] = [ arr[child], arr[parent] ];
-
-      //update parent with child and get child of updated parent
-      parent = child;
-      child = getChild(parent);
-    }
-  }
-  return arr;
-}
-*/
-
 
 function heapsort(arr) {
-
-
-
-
-
   //we know heap condition is met when there is only one item in it
   //start at 1 to represent index 0
   let heapLength = 1;
---
+++ solved/sorting/mergesort.js
@@ -0,0 +1,55 @@
+let joinArrays = function(arr1, arr2) {
+  //set up 2 pointers
+  let p1 = 0;
+  let p2 = 0;
+  let result = []; //array that we will build up
+
+  if (arr1 === undefined) {
+    return arr2;
+  }
+  else if (arr2 === undefined) {
+    return arr1;
+  }
+
+  //if there are indices to traverse, compare arr1 and arr2 and place the lesser values in the result
+  while (arr1[p1] !== undefined && arr2[p2] !== undefined) {
+    //stable: elements that appear earlier in array if equal to others are represented earlier in the result
+    if (arr1[p1] <= arr2[p2]) {
+      result.push(arr1[p1]);
+      p1++;
+    }
+    else {
+      result.push(arr2[p2]);
+      p2++;
+    }
+  }
+
+  //pointer reaches end of one array first, add the rest of the other array to result
+  if (p1 === arr1.length) {
+    result = result.concat(arr2.slice(p2));
+  }
+  else if (p1 === arr2.length) {
+    result = result.concat(arr1.slice(p1));
+  }
+
+  return result;
+}
+
+let mergeSort = function(input) {
+  if (input.length <= 1) {
+    return input;
+  }
+
+  let midIndex = Math.floor(input.length / 2);
+  let left = input.slice(0, midIndex);
+  let right = input.slice(midIndex, input.length);
+
+  return joinArrays(mergeSort(left), mergeSort(right));
+}
+
+let i = 1000;
+let test = [];
+while (i--) {
+  test.push(Math.floor(1000* Math.random()));
+}
+console.log(mergeSort(test));
