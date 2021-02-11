bb9928e80d4aed544d9d81b1af68ebe763472bac
Kitan Garcia <kitangar@gmail.com>
2021-02-10 19:35:21 -0800

    complete list forests for pachama using bfs

--- listForestsPachama.js
+++ listForestsPachama.js
@@ -24,6 +24,8 @@ function listForests(array) {
 
       //if the value is a 1 that has not been visited, perform bfs
       if (map[i][j] == 1 && !visited.has(i + "_" + j)) {
+        console.log(i, j);
+        visited.add(i + "_" + j);
         //result.push([i, j]);
         result = result.concat(bfs(map, visited, i, j));
       }
@@ -38,15 +40,17 @@ function bfs(map, visited, i, j) {
   queue.push([i, j]);
 
   while (queue.length > 0) {
-    let current = queue.shift(); //should it be named tuple?
+    let current = queue.shift();
     result.push(current);
 
     let neighbors = getNeighbors(map, current[0], current[1]);
+    /*
     console.log("Queue: " + queue);
+    console.log("Current: " + current);
     console.log("Visited: ");
     console.log(visited);
-    console.log("Current: " + current);
     console.log("Neighbors: " + neighbors);
+    */
 
     for (let tuple of neighbors) {
       if (!visited.has(tuple[0] + "_" + tuple[1])) {
@@ -62,12 +66,8 @@ function getNeighbors(map, i, j) {
   let neighbors = [];
   let tuple = [];
 
-  //check i and j for bounds of map
-  console.log(map.length);
-  console.log(map[0].length);
 
-  //shouldn't this be  i <= map.length?????????????
-  if (i <= map.length) {
+  if (i < map.length - 1) {
     if (map[i + 1][j] == 1) {
       neighbors.push([i + 1, j]);
     }
@@ -79,7 +79,7 @@ function getNeighbors(map, i, j) {
     }
   }
 
-  if (j <= map[0].length) {
+  if (j < map[0].length - 1) {
     if (map[i][j + 1] == 1) {
       neighbors.push([i, j + 1]);
     }
@@ -87,13 +87,10 @@ function getNeighbors(map, i, j) {
   return neighbors;
 }
 
-
 let input =  [[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
               [ 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0],
               [ 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1],
-              [ 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
-//              [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
-//
+              [ 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]];
 
 let input2 =  [[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
               [ 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0],
@@ -101,4 +98,22 @@ let input2 =  [[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
               [ 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
               [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
 
-console.log(listForests(input2));
+let input3 =  [[ 1, 1, 0, 0, 0, 0, 0, 1, 1],
+              [ 1, 0, 0, 0, 0, 0, 0, 0, 1],
+              [ 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [ 1, 0, 0, 0, 0, 0, 0, 0, 1],
+              [ 1, 1, 0, 0, 0, 0, 0, 1, 1]];
+
+let input4 =  [[ 1, 1, 0, 1, 1, 0, 0, 1, 1]];
+
+
+let input5 =  [[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [ 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0],
+              [ 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1],
+              [ 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]];
+
+//console.log(listForests(input));
+//console.log(listForests(input2));
+//console.log(listForests(input3));
+//console.log(listForests(input4));
+console.log(listForests(input5));
