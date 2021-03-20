420d78864e65ac67b6efc98484a24335e35e5876
Kitan Garcia <kitangar@gmail.com>
2021-03-19 18:23:15 -0700

    add traveling salesman

---
+++ travelingSalesman.js
@@ -0,0 +1,12 @@
+/*
+ * What is the shortest path to visit all cities in a set of cities
+ *
+ * For example, a roadtrip to reach all the capitals in the US
+ */
+
+
+/*
+ * Today:
+ *   Implement the find closest neighbor
+ *   Compare the greedy approach to the correct answer
+ */
