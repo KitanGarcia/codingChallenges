2b78a5d874ed7d91fec717c649c122e1b2dc6b13
Kitan Garcia <kitangar@gmail.com>
2022-08-03 19:57:21 -0700

    complete transposeMatrix

---
+++ interviewPrep/solved/easy/arrays/transposeMatrix.js
@@ -0,0 +1,42 @@
+/*
+ * Leetcode 867
+Given a 2D integer array matrix, return the transpose of matrix.
+
+The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+
+Example 1:
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+Example 2:
+
+Input: matrix = [[1,2,3],[4,5,6]]
+Output: [[1,4],[2,5],[3,6]]
+ */
+
+const transpose = function (matrix) {
+  let transpose = [];
+
+  for (let j = 0; j < matrix[0].length; j++) {
+    let row = [];
+    for (let i = 0; i < matrix.length; i++) {
+      row.push(matrix[i][j]);
+    }
+    transpose.push(row);
+  }
+  return transpose;
+};
+
+console.log(
+  transpose([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ])
+);
+console.log(
+  transpose([
+    [1, 2, 3],
+    [4, 5, 6],
+  ])
+);
